21053a3c6358d443abe9e803a7e80b7f719fa681
Enne Jim Pessoa <ennefofe10@gmail.com>
2026-01-29 09:37:12 -0300

    fix: excessive URL redirection

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://ennetechdesign.vercel.app/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
